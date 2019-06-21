b278a7b9b0d33a5ccd1aaa9672d32df16cd106cb
Bo Peng <pengbo37877@gmail.com>
2019-06-21 16:54:40 +0800

    auto deploy

---
+++ Envoy.blade.php
@@ -0,0 +1,11 @@
+@servers(['web-1' => 'root@120.25.207.77', 'web-2' => 'root@120.79.191.149', 'web-3' => 'root@47.106.185.192'])
+
+@task('deploy', ['on' => ['web-1', 'web-2', 'web-3'], 'parallel' => true])
+    cd /var/www/lb-test
+    git pull origin master
+    chown -R nginx:nginx .
+    chmod -R 777 storage
+    echo "" > storage/logs/laravel.log
+    composer install --no-dev
+    php artisan migrate
+@endtask
