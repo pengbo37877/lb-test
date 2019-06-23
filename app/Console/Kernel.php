a123fc66b28c556a3c5d50baeb41a4b9d1531522
Bo Peng <pengbo37877@gmail.com>
2019-06-23 19:38:22 +0800

    add LBJob

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->job(new LBJob())->everyMinute()->withoutOverlapping()->onOneServer();
+        $schedule->job(new LBJob())->everyMinute()->onOneServer();
     }
 
     /**
