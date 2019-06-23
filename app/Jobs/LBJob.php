16a791a7165ec385de782f39952ab0aff2aad3b8
Bo Peng <pengbo37877@gmail.com>
2019-06-23 21:07:58 +0800

    job

--- app/Jobs/LBJob.php
+++ app/Jobs/LBJob.php
@@ -13,6 +13,8 @@ class LBJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    public $deleteWhenMissingModels = true;
+
     /**
      * Create a new job instance.
      *
